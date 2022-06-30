8bfecb7013de6a32cde1c8ca81c2c5120785c699
Standard <oanlara8@gmail.com>
2022-06-30 15:11:45 -0600

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -20,28 +20,28 @@ const validateLogin = [
 const router = express.Router();
 
 
-router.post(
-    '/',
-    async (req, res, next) => {
-      const { credential, password } = req.body;
+// router.post(
+//     '/',
+//     async (req, res, next) => {
+//       const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+//       const user = await User.login({ credential, password });
 
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
+//       if (!user) {
+//         const err = new Error('Login failed');
+//         err.status = 401;
+//         err.title = 'Login failed';
+//         err.errors = ['The provided credentials were invalid.'];
+//         return next(err);
+//       }
 
-      await setTokenCookie(res, user);
+//       await setTokenCookie(res, user);
 
-      return res.json({
-        user
-      });
-    }
-  );
+//       return res.json({
+//         user
+//       });
+//     }
+//   );
 
 
 // Log out

f459ba941676e2dce3c44430358116d3d7c3c518
Standard <oanlara8@gmail.com>
2022-07-10 15:55:13 -0600

    triage

--- backend/db/models/playlists.js
+++ backend/db/models/playlists.js
@@ -12,10 +12,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Playlist.belongsTo(models.User, { foreignKey : 'userId'})
       //Playlist.hasMany(models.PlaylistSong);//, { sourceKey: 'id', foreignKey: 'playlistId' });
-      Playlist.belongsToMany(
-          models.Song,
-          { through: models.PlaylistSong}
-      );
+      // Playlist.belongsToMany(
+      //     models.Song,
+      //     { through: models.PlaylistSong}
+      // );
     }
   }
   Playlist.init({

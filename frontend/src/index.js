7a91d6cbd51866ba272fc11a1f7d869be4167eb5
Standard <oanlara8@gmail.com>
2022-08-23 11:45:04 -0600

    authme phase 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import {Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 
 import App from './App';
@@ -18,6 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 

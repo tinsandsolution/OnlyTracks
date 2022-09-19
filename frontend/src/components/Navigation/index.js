7f06621aed0d73cc6e99fd29221921c2278e4b16
Standard <oanlara8@gmail.com>
2022-09-19 14:07:46 -0600

    css for main

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-
+import splash from '../../assets/dream_Kandinsky.png'
+import logo from '../../assets/otlogo2.png'
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
@@ -15,19 +16,27 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <div className="not-logged-in">
+          <li><NavLink to="/login">Log In</NavLink></li>
+          <li><NavLink to="/signup">Sign Up</NavLink></li>
+        </div>
       </>
     );
   }
 
+  const splash = (<img src={splash} alt="splash"></img>)
+
   return (
-    <ul>
+    <>
+    <ul className='navbar'>
+      <li><img src={logo} className="navlogo" alt="logo"></img></li>
       <li>
         <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
       </li>
+      {isLoaded && sessionLinks}
     </ul>
+    {sessionUser && splash}
+    </>
   );
 }
 

f9383fee57acd98e5c90f5805e1479e6090ac1db
Standard <oanlara8@gmail.com>
2022-09-22 09:20:57 -0600

    comment section

---
+++ frontend/src/components/CommentSection/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from "../../store/session";
+import * as songActions from "../../store/songs"
+// import { Redirect } from "react-router-dom";
+
+function CommentSection(){
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(songActions.getSongs());
+    },[dispatch])
+
+    const songs = Object.values(useSelector(state => state.songs)).map(song => <li>{song.title}</li>)
+
+    console.log("printing songs")
+    console.log(songs)
+
+    return (
+        <>
+        <p>dasdasd</p>
+        <ul>
+            {songs}
+        </ul>
+        </>
+    )
+}
+
+export default CommentSection;

441a49122fb7304ea30e73fa0bb1bc5426076843
Standard <oanlara8@gmail.com>
2022-12-12 14:44:22 -0700

    https enforced

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,11 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 
+// automatically directes to https
+if (window.location.protocol !== "https:") {
+  window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+}
+
 function Root() {
   return (
     <Provider store={store}>
